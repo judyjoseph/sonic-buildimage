49d62de033b51c12046976d97265409c0210d4df
Judy Joseph <jujoseph@microsoft.com>
2022-08-05 12:55:33 -0700

    Update

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -12,7 +12,7 @@ $(BRCM_XGS_SAI_DEV)_URL = "$(LIBSAIBCM_XGS_URL_PREFIX)/$(BRCM_XGS_SAI_DEV)"
 
 # SAI module for DNX Asic family
 BRCM_DNX_SAI = libsaibcm_dnx_$(LIBSAIBCM_DNX_VERSION)_amd64.deb
-$(BRCM_DNX_SAI)_URL = "$(LIBSAIBCM_DNX_URL_PREFIX)/$(BRCM_DNX_SAI)"
+$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/7.0/7.1.0.0-7/libsaibcm_dnx_7.1.0.0-7_amd64.deb?sv=2020-10-02&st=2022-07-23T02%3A00%3A55Z&se=2026-07-24T02%3A00%3A00Z&sr=b&sp=r&sig=AEgooC3VpB3ee%2FcxtzUIWtoaFpWbdoYltuqEXGkrPwQ%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_XGS_SAI)
 SONIC_ONLINE_DEBS += $(BRCM_DNX_SAI)
